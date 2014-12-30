8feea24e4488f6dc2d45433b56907db2e10de7e4
Yossi Zini <yzini@salesforce.com>
2014-12-30 16:31:42 +0200

    node

--- app.js
+++ app.js
@@ -9,22 +9,17 @@ var express = require('express');
 var user = require('./routes/samples/user');
 var http = require('http');
 var path = require('path');
+var errors = require('./lib/errors.js');
+var config = require('./lib/config.js');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3001);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+config.addToApp(app);
+
 
 var timeout = require('connect-timeout'); //express v4
 
@@ -40,39 +35,17 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+var restEndPoints = require('./lib/restEndPoints.js');
 
-app.get('/users', user.list);
-app.post('/signedrequest', processSignedRequest);
-
-
-
-
-var decode = require('salesforce-signed-request');
+restEndPoints.addToApp(app);
 
 
 
-function processSignedRequest(req, res) {
-    var shipment = new Shipment();
-    try {
-        var json = func(req.body.signed_request, config.APP_SECRET);
-        res.render("index", json);
-    } catch (e) {
-        res.render("error", {
-            "error": errors.SIGNED_REQUEST_PARSING_ERROR
-        });
-    }
-}
 
-func = function processSignedRequest(signedRequest, APP_SECRET) {
-    var sfContext = decode(signedRequest, APP_SECRET);
-    //console.log(sfContext);
-    return {
-        oauthToken: sfContext.client.oauthToken,
-        instanceUrl: sfContext.client.instanceUrl
 
-    }
-    //        warehouseId: sfContext.context.environment.parameters.id //sent as parameters via visualForce parameters
-};
+
+
+
 
 
 var func=function(){
